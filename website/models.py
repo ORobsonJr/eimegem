5c7bcf8469be11ffa2cabc401ac5dabe828958d1
Robson J <robsonjrservices@gmail.com>
2023-05-01 14:14:22 -0300

    Word object created in models

--- website/models.py
+++ website/models.py
@@ -5,3 +5,6 @@ from django.db import models
 """
 Models is empty because we don't wan't to store anything
 """
+class Word(models.Model):
+    word = models.CharField(max_length=30, primary_key=True)
+    frequency = models.IntegerField()
