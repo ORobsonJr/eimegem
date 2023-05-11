8e79f3c05234c497106ab655e71910af05108cc3
Robson J <robsonjrservices@gmail.com>
2023-05-11 07:55:26 -0300

    New feature refresh created --> scrape words from pdfs and insert inside database

--- website/models.py
+++ website/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-"""
-Models is empty because we don't wan't to store anything
-"""
-class Word(models.Model):
-    word = models.CharField(max_length=30, primary_key=True)
-    frequency = models.IntegerField()
--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import convert_image
-from .views import PAGE
+from .views import refresh_learn_ai, Refresh, PAGE, convert_image
 
 API_VERSION = 'v1/'
 
 urlpatterns = [
     path('', PAGE.as_view()),
+    path('refresh', Refresh.as_view()),
     path(API_VERSION + 'convertImage', convert_image),
+    path(API_VERSION + 'refresh_ai', refresh_learn_ai)
 ]
