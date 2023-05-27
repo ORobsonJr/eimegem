709f2524886e9dbcdfa9b9c05d2a7f9fe3dc7978
Robson J <robsonjrservices@gmail.com>
2023-05-27 20:43:19 -0300

    [Database]: add portuguese words as values; change frequency database field to 'default=0' in models and after make migrations

--- converter/models.py
+++ converter/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Word(models.Model):
     word = models.CharField(max_length=30, primary_key=True)
-    frequency = models.IntegerField()
+    frequency = models.IntegerField(default=0)
