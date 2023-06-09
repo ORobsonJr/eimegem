a0e76d7a0db4c15a2bd131930d0dfe69655a4468
Robson J <robsonjrservices@gmail.com>
2023-06-09 10:25:44 -0300

    setup updated, create .env file and set API_KEY

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
-import os
-from setuptools import setup, find_packages
+from os import path
 
 """
-Install requirements and eimegem as package
+Create and config .env file
 """
 
-REQUIREMENTS = 'requirements.txt'
+DIR_PATH = dir_path = path.dirname(path.realpath(__file__))
+ENV_file = path.join(DIR_PATH, '.env')
 
-if __name__ == '__main__':
-    os.system(f'pip3 install -r {REQUIREMENTS}')
-    setup(
-        name='eimegem',
-        packages=find_packages(),
-        author='Robson J',
-        version=open('VERSION', 'r').read()
-    )
+if path.exists(ENV_file) is False:
+    API_KEY = input("\033[36m[ALERT]\033[0m Por favor insira sua chave API do chat-gpt aqui: ")
+
+    try:
+        with open(ENV_file, 'w') as f:
+            f.write('API_KEY=' + API_KEY)
+            print("\033[32m[SUCCESS]\033[0m Arquivo .env criado com sucesso!\nem caso de chave errada ou para reconfigurar por favor exclua o arquivo .env e execute novamente.")
+    
+    except Exception as e:
+        print("\033[31m[ERROR]\033[0m ", e)
 
 else:
-    raise Exception('Please ensure that __name__ is __main__')
+    print("\033[31m[ERROR]\033[0m O arquivo .env já existe, exclua ele caso queira reconfigurar.")
